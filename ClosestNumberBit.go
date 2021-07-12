e534b3c329e60e8d4e3d30633fc9dd2bb03eae92
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-07-12 23:07:49 +0300

    Added a task

---
+++ ClosestNumberBit.go
@@ -0,0 +1,65 @@
+/*
+32-bit non-negative integer.
+Find the nearest smaller non-negative integer with the same number of bits
+*/
+
+package main
+
+import "fmt"
+
+func getTrailingBitsCount(number int) (int, int) {
+	if number == 0 {
+		return -1, -1
+	}
+
+	trailingZeroesCount := 0
+	trailingOnesCount := 0
+
+	for number & 1 == 1 {
+		trailingOnesCount++
+		number = number >> 1
+	}
+
+	if number == 0 {
+		return -1, -1
+	}
+
+	for number & 1 == 0 {
+		trailingZeroesCount++
+		number = number >> 1
+	}
+
+	return trailingZeroesCount, trailingOnesCount
+}
+
+func clearRightBits(number, amountClearRightBits int) int {
+	return (-1 << amountClearRightBits) & number
+}
+
+func setBits(number, start, amountSetBits int) int {
+	return (((1 << amountSetBits) - 1) << start) | number
+}
+
+func getClosestLowerNumber(number int) int {
+	amountZeroes, amountOnes := getTrailingBitsCount(number)
+	if amountZeroes == -1 && amountOnes == -1 {
+		return -1
+	}
+
+	bitsToClear := amountZeroes + amountOnes + 1
+	number = clearRightBits(number, bitsToClear)
+	number = setBits(number, amountZeroes - 1, amountOnes + 1)
+
+	return number
+}
+
+func main() {
+	fmt.Print("Please, enter a non-negative number: ")
+	var number int
+	_, err := fmt.Scanf("%d", &number)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(getClosestLowerNumber(number))
+}
