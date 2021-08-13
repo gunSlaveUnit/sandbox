cdb5820f34db6da23bd21cd194f54c4c5342d81c
gunSlaveUnit <rp34789t5409yvt.protonmail.com>
2021-08-13 21:15:42 +0300

    Added lines parsing

--- quizGame/main.go
+++ quizGame/main.go
@@ -7,6 +7,22 @@ import (
 	"encoding/csv"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem {
+			q: line[0], 
+			a: line[1],
+		}
+	}
+	return ret
+}
+
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a сsv file in the format of 'question, answer'")
 	flag.Parse()
@@ -19,7 +35,10 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV file")
 	}
-	fmt.Println(lines)
+	problems := parseLines(lines)
+	for i := 0; i < len(problems); i++ {
+		fmt.Println(problems[i])
+	}
 }
 
 func exit(message string) {
