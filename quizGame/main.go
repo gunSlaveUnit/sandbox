98adeece238a6f7d30e9a01f6b1aaa5f011ff4ec
gunSlaveUnit <rp34789t5409yvt.protonmail.com>
2021-08-13 12:21:19 +0300

    Added file opening

--- quizGame/main.go
+++ quizGame/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Work")
+	csvFilename := flag.String("csv", "problems.csv", "a сsv file in the format of 'question, answer'")
+	flag.Parse()
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open %s", *csvFilename))
+	}
+	_ = file
+}
+
+func exit(message string) {
+	fmt.Println(message)
+	os.Exit(1)
 }
