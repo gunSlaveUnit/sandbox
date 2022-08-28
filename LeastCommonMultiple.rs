e14e8166b27a6e2c6df103d08ce83d6a04558098
gunSlaveUnit <58048263+gunSlaveUnit@users.noreply.github.com>
2022-08-28 21:33:03 +0300

    Create LeastCommonMultiple.rs

---
+++ LeastCommonMultiple.rs
@@ -0,0 +1,25 @@
+fn gcd(mut a: u32, mut b: u32) -> u32 {
+    /// GCD - Greatest Common Divisor.
+    /// Calculated by the Euclidean algorithm.
+    loop {
+        if b == 0 {
+            break;
+        }
+
+        let remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+
+    return a;
+}
+
+fn lcm(a: u32, b: u32) -> u32 {
+    /// LCM - Least Common Multiple.
+    ///	Formula: LCM(A, B) = A * B / GCD(A, B).
+    return a / gcd(a, b) * b;
+}
+
+fn main() {
+    println!("{}", lcm(60, 45));
+}
