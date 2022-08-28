12a53636e324cce5fd9e69e1586b36b106436133
gunSlaveUnit <58048263+gunSlaveUnit@users.noreply.github.com>
2022-08-28 21:14:55 +0300

    Update Greatest common factor.cpp
    Added Euclid's algorithm (python)

--- Greatest common factor.cpp
+++ Greatest common factor.cpp
@@ -1,3 +1,30 @@
+/*
+from time import perf_counter
+
+def time_measure(derived):
+	def wrapper(*args, **kwargs):
+		start = perf_counter()
+		result = derived(*args, **kwargs)
+		end = perf_counter()
+		print(f"Done for {end - start}")
+	return wrapper
+
+
+@time_measure
+def GCD(first, second):
+	"""
+	Finds a greatest common divisor for the first and second numbers.
+	Uses Euclid's algorithm.
+	"""
+
+	while(second != 0):
+	remainder = first % second
+	first = second
+	second = remainder
+
+	return first
+*/
+
 #include <iostream>
 
 int main()
